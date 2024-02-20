de87284cd40d3f24c5c0b96be6fefffd163fd023
Jan Krüger <jan-luca.krueger@web.de>
2024-02-20 11:12:45 +0100

    Added file containing the class used for representing pixels.

---
+++ src/classes/pixel.py
@@ -0,0 +1,57 @@
+"""File containing the Pixel class."""
+
+class Pixel:
+    """
+    A class representing one Pixel.
+
+    Attributes
+    ----------
+    red_value: int
+        Value of the primary colour red.
+    green_value: int
+        Value of the primary colour green.
+    blue_value: int
+        Value of the primary colour blue.
+    grey_value: int
+        Grey value of the pixel.
+
+    Methods
+    -------
+    get_gray_value
+        Return the grey value of the pixel.
+
+    """
+
+    def __init__(self, red_value: int, green_value: int, blue_value: int) -> None:
+        """
+        Initialize one Pixel object with the given attributes.
+
+        Paramneters
+        -----------
+        red_value: int
+            Value of the primary colour red.
+        green_value: int
+            Value of the primary colour green.
+        blue_value: int
+            Value of the primary colour blue.
+
+        """
+        self.red_value: int = red_value
+        self.green_value: int = green_value
+        self.blue_value: int = blue_value
+
+        # Determine the grey value of the pixel
+        self.grey_value: int = int(sum([
+            self.green_value, self.blue_value, self.green_value]) / 3)
+
+    def get_gray_value(self) -> int:
+        """
+        Return the grey value of the pixel.
+
+        Returns
+        -------
+        self.grey_value: int
+            Grey value of the pixel.
+
+        """
+        return self.grey_value
