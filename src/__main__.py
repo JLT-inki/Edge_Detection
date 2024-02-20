bdb8c8c31397342f97c622e5ff3f6186815bfed6
Jan Krüger <jan-luca.krueger@web.de>
2024-02-20 14:18:54 +0100

    Removed Filter class. Instead filters are now accessed via a dictionary in the main file.

--- src/__main__.py
+++ src/__main__.py
@@ -6,6 +6,13 @@ import time
 # Import used classes
 from classes.pixel import Pixel
 from classes.image import Image
+from classes.matrix import Matrix
+
+# Dictionary for the different filters
+DIFFERENTIAL_FILTERS: dict[str, Matrix] = {
+    'soebel': Matrix([[-1, 0, 1], [-2, 0, 2], [-1, 0, 1]]),
+    'prewitt': Matrix([[-1, 0, 1], [-1, 0, 1], [-1, 0, 1]])
+}
 
 # Path to the image
 PATH_TO_IMAGE: str = "../images/Auto.jpg"
