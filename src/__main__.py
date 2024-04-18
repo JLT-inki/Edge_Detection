f1a17fab09d7f1c1dc6f0236247c5429c0db285b
Jan Krueger <jan-luca.krueger@web.de>
2024-04-18 14:56:48 +0200

    Updated main file of the project.

--- src/__main__.py
+++ src/__main__.py
@@ -1,29 +1,34 @@
 """Main file of the project."""
 
 import sys
-import time
 
 # Import used classes
-from classes.pixel import Pixel
 from classes.image import Image
 from classes.matrix import Matrix
 
 # Dictionary for the different filters
 DIFFERENTIAL_FILTERS: dict[str, Matrix] = {
-    'soebel': Matrix([[-1, 0, 1], [-2, 0, 2], [-1, 0, 1]]),
+    'soebel': Matrix([[-1.0, 0.0, 1.0], [-2.0, 0.0, 2.0], [-1.0, 0.0, 1.0]]),
     'prewitt': Matrix([[-1, 0, 1], [-1, 0, 1], [-1, 0, 1]])
 }
 
-# Path to the image
-PATH_TO_IMAGE: str = "../images/Bild1.jpg"
+# Path to the images
+PATH_TO_IMAGES: dict[int, str] = {
+    1: "../images/Car.jpg",
+    2: "../images/Church.jpg",
+    3: "../images/Circle.jpg",
+    4: "../images/Sydney.jpg"
+}
 
 def main() -> int:
-    image_normal: Image = Image.read_image(PATH_TO_IMAGE)
-    #image_normal.show_image()
-    a = Image(image_normal.traverse_vertically(DIFFERENTIAL_FILTERS['soebel']))
-    a.show_image()
+    """Execute the project."""
+    # Read the original image
+    image_normal: Image = Image.read_image(PATH_TO_IMAGES[1])
 
-        
+    # Traverse the image and show it
+    image_traversed = Image(image_normal.traverse(DIFFERENTIAL_FILTERS['soebel']))
+    image_traversed.show_image()
+
     # Return Exitcode 0 indicating success
     return 0
 
