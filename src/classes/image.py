8e34fb16c4f013ba9dea0bc72490031cc6e61fc9
jkr6254 <Jan.Krueger@philotech.de>
2024-04-22 08:18:46 +0200

    Updated the traverse and create_pixel_matrix method of the Image class to be more generic.

--- src/classes/image.py
+++ src/classes/image.py
@@ -135,7 +135,8 @@ class Image:
         plt.show()
 
     @staticmethod
-    def create_pixel_matrix(values: list[list[float]], pos: tuple[int, int]) -> Matrix:
+    def create_pixel_matrix(values: list[list[float]], pos: tuple[int, int],
+                            size: tuple[int, int]) -> Matrix:
         """
         Create a matrix including the pixel and the 8 surrounding pixels.
 
@@ -152,13 +153,13 @@ class Image:
             Matrix containing the pixel and the 8 surrounding pixel.
 
         """
+        # Get the indices for the height and width
+        height: int = int(size[0] / 2)
+        width: int = int(size[1] / 2)
+
         pixel_matrix: Matrix = Matrix([
-            [values[pos[0] - 1][pos[1] - 1], values[pos[0] - 1][pos[1]],
-             values[pos[0] - 1][pos[1] + 1]],
-            [values[pos[0]][pos[1] - 1], values[pos[0]][pos[1]],
-             values[pos[0]][pos[1] + 1]],
-            [values[pos[0] + 1][pos[1] - 1], values[pos[0] + 1][pos[1]],
-             values[pos[0] + 1][pos[1] + 1]]])
+            [values[row][col] for col in range(pos[1] - width, pos[1] + width + 1)]
+            for row in range(pos[0] - height, pos[0] + height + 1)])
 
         return pixel_matrix
 
@@ -177,31 +178,37 @@ class Image:
             Differentiated pixels after vertical transverse.
 
         """
+        # Get the grayscale values of the image
         gray_values: list[list[float]] = self.get_gray_values()
+
+        # Initialize the return value
         pixels_differentiated: list[list[Pixel]] = []
+
+        # Get the number of rows and columns of the differential filter
+        rows_filter: int = differential_filter.get_number_of_rows()
+        cols_filter: int = differential_filter.get_number_of_columns()
 
         # Calculate the border to ignore; Tuple[rows, columns]
         border: tuple[int, int] = (
-            differential_filter.get_number_of_rows() - 2
-            if differential_filter.get_number_of_rows() > 0 else 0,
-            differential_filter.get_number_of_columns() - 2
-            if differential_filter.get_number_of_columns() > 0 else 0
-        )
+            rows_filter - 2 if rows_filter > 3 else 1,
+            cols_filter - 2 if cols_filter > 3 else 1)
 
-        # Ignore the first and last pixel in each row and column
+        # Ignore the border of the image
         for row_count, row in enumerate(gray_values[border[0]:-border[0]]):
             print(row_count)
             pixels_differentiated.append([])
 
             for col_count in range(len(row[border[1]:-border[1]])):
-                # Increment row/column by 1 count as the first row/column was skipped
-                pixel_matrix: Matrix = Image.create_pixel_matrix(
-                    gray_values, (row_count, col_count))
+                # Get the sub matrix for both directions
+                pixel_matrix_x: Matrix = Image.create_pixel_matrix(
+                    gray_values, (row_count, col_count), (rows_filter, cols_filter))
+                pixel_matrix_y: Matrix = Image.create_pixel_matrix(
+                    gray_values, (row_count, col_count), (cols_filter, rows_filter))
 
                 # Calculate the differentiated pixel value at the current position
-                gradient_x: float = pixel_matrix.apply_filter(
+                gradient_x: float = pixel_matrix_x.apply_filter(
                     differential_filter).get_sum()
-                gradient_y: float = pixel_matrix.apply_filter(
+                gradient_y: float = pixel_matrix_y.apply_filter(
                     differential_filter.transpose()).get_sum()
                 gradient_absolute: int = int(
                     (abs(gradient_x) ** 2 + abs(gradient_y) ** 2) ** 0.5)
